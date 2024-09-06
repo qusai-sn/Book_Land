7f30eb736133abf51dc10a76f3f14de55e11383c
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-09-06 23:53:46 +0300

    add user controller

--- BookLand/Controllers/UserProfileController.cs
+++ BookLand/Controllers/UserProfileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BookLand.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookLand.Controllers
@@ -7,5 +8,17 @@ namespace BookLand.Controllers
     [ApiController]
     public class UserProfileController : ControllerBase
     {
+        private readonly MyDbContext _db;
+        public UserProfileController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers() {
+
+            var users = _db.Users.ToList();
+            return Ok(users);    
+        }
     }
 }
