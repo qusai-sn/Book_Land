ad421e481f26f9035cdbb4cf4a15fdf142d81b24
qusai_sn <qusai_sn@outlook.com>
2024-09-06 22:32:57 +0300

    programm cs

--- BookLand/Program.cs
+++ BookLand/Program.cs
@@ -1,3 +1,6 @@
+using BookLand.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// Add services to the container.
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("YourConnectionString")));
+
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
