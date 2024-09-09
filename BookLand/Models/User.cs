f06c3e1b4b789099562ad70bbaaccbeab1399e04
qusai_sn <qusai_sn@outlook.com>
2024-09-09 17:37:48 +0300

    scafoolding

--- BookLand/Models/User.cs
+++ BookLand/Models/User.cs
@@ -19,7 +19,7 @@ public partial class User
 
     public string? Address { get; set; }
 
-    public string? PhoneNumber { get; set; }    
+    public string? PhoneNumber { get; set; }
 
     public int? Points { get; set; }
 
