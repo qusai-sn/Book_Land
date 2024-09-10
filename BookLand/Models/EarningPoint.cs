2c8f697aa2dd7d555efb9c5526c9fb62d4aa8d5b
qusai_sn <qusai_sn@outlook.com>
2024-09-10 09:07:19 +0300

    scaffolding from another pc

---
+++ BookLand/Models/EarningPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLand.Models;
+
+public partial class EarningPoint
+{
+    public int Id { get; set; }
+
+    public int? SocialMediaShare { get; set; }
+
+    public int? BookPurchase { get; set; }
+
+    public int? InviteFriend { get; set; }
+}
---
+++ BookLand/Models/UserDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookLand.Models;
+
+public partial class UserDetail
+{
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string? ProfessionalTitle { get; set; }
+
+    public string? Languages { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Postcode { get; set; }
+
+    public virtual User? User { get; set; }
+}
